aa9d4bbb6764a180a1644ab6b609c1b858b724c6
Wajid <errormrcoder@example.com>
2025-07-19 08:17:04 +0500

    Basic complete app.

--- Src/main.py
+++ Src/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Home api or home page 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# Second api 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
